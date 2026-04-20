552ad4b467a0db4a79c239be342a76b3cdaf566d
PRSTO-Mkittok <36708471+SEI-Mkittok@users.noreply.github.com>
2026-04-20 02:09:43 -0500

    Fix dialog aria descriptions and dashboard chart sizing (#181)

--- web/src/components/ui/Dialog.tsx
+++ web/src/components/ui/Dialog.tsx
@@ -25,6 +25,7 @@ export function DialogOverlay({
 export function DialogContent({
   className,
   children,
+  ['aria-describedby']: ariaDescribedBy,
   ...props
 }: React.ComponentPropsWithoutRef<typeof RadixDialog.Content>) {
   return (
@@ -35,6 +36,7 @@ export function DialogContent({
           'fixed left-[50%] top-[50%] z-50 grid w-[calc(100%-2rem)] max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 bg-white p-6 shadow-xl duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-xl',
           className
         )}
+        aria-describedby={ariaDescribedBy}
         {...props}
       >
         {children}
